77ba0f5f995750912b72b01c1ff549cfa0d5857b
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-07-21 15:27:00 -0300

    .

--- logic.js
+++ logic.js
@@ -1,27 +1,32 @@
 // writing excercise code here random comment in script
 /* random comment in paragraph */
-$(document).ready(fuction(){
+$(document).ready(function(){
+//Start
 
-var fruits = ["Apple", "Orange", "Banana", "Pineapple"];
 
-function redrawList(){
-  var list = $("#fruitList");
+   var fruits = ["Apple", "Orange", "Banana", "Pineapple"];
 
-  list.html("");
+   function redrawList(){
+       var list = $("#fruitList");
 
-  $.each(fruits,function(index,value){
-      $("<li/>").text(value).appendTo(list);
-  });
+       list.html("");
 
-}
+       $.each(fruits,function(index,value){
+         $("<li/>").text(value).appendTo(list);
+       });
 
-redrawList();
+    };
 
-$("#addFruitButton").click(function(){
-  var fruitText= $("#fruitTextInput").val();
-  fruits.push(fruitText);
-  redrawList();
-  alert(fruitText);
-});
+    $("#addFruitButton").click(function(){
+      var fruitText= $("#fruitTextInput").val();
+      fruits.push(fruitText);
+      redrawList();
+      alert(fruitText);
+    });
 
+   redrawList();
+
+
+
+//End
 })
