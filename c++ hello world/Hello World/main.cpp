f142949a37a587f5131c50bb5dffd881aca93f59
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-07-23 08:11:24 -0300

    .

--- c++ hello world/Hello World/main.cpp
+++ c++ hello world/Hello World/main.cpp
@@ -2,20 +2,22 @@
 
 using namespace std;
 
-void printHello() {
-  // local variable
-  string helloString = "hello!";
-  cout << helloString;
-}
-
-void test() {
-  int a = 0;
-  cout << "test "<< a;
-}
-
 int main()
 {
-  printHello() ;
-  test() ;
+  int number =0;
+
+  cout << "Please input a number:" <<endl;
+  cin >> number;
+
+  if(number <10){
+    cout << "Your number is LESS than 10!" <<endl;
+  }
+  else if(number >= 10 && number > 15){
+    cout << "The number is greater of equal than 10 and below 15" <<endl;
+  }
+  else{
+    cout << "The number is greater or equal to 15" <<endl;
+  }
+
   return 0;
 }
