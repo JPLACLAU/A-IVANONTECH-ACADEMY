eabbb6168f9aa5f3a3d706884f5273fc54b135f0
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-07-23 06:20:34 -0300

    -

--- c++ hello world/Hello World/main.cpp
+++ c++ hello world/Hello World/main.cpp
@@ -9,6 +9,12 @@ int main()
     int a = 5;
     int b = 3;
     int c = a+b;
+    //second type - boolean
+    bool d = true;
+    bool e = false;
+    //true = 1
+    //False = 0
+    bool k = d||e;
     cout<<"Hello everyone"<< c;
     return 0;
 }
