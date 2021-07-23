381b206b34e6eda9d70dab6f903800b9d82d219e
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-07-23 06:29:24 -0300

    .

--- c++ hello world/Hello World/main.cpp
+++ c++ hello world/Hello World/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     // Variable have types
-    //first type - (integer)
+    //first type - (integer) only hole number, 0,5 wont work
     int a = 5;
     int b = 3;
     int c = a+b;
@@ -15,6 +15,12 @@ int main()
     //true = 1
     //False = 0
     bool k = d&&e;
+    //third type is double
+    double h = 0.9;
+    double j = 0.3;
+
+    int result = h+j;
+    
     cout<<"Hello everyone"<< k;
     return 0;
 }
