6443becaed94f5e8ce59d66364ab8c5f301b565e
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-07-23 06:35:02 -0300

    .

--- c++ hello world/Hello World/main.cpp
+++ c++ hello world/Hello World/main.cpp
@@ -4,23 +4,14 @@ using namespace std;
 
 int main()
 {
-    // Variable have types
-    //first type - (integer) only hole number, 0,5 wont work
-    int a = 5;
-    int b = 3;
-    int c = a+b;
-    //second type - boolean
-    bool d = true;
-    bool e = false;
-    //true = 1
-    //False = 0
-    bool k = d&&e;
-    //third type is double
-    double h = 0.9;
-    double j = 0.3;
 
-    int result = h+j;
-    
-    cout<<"Hello everyone"<< k;
+/*
+1. get the name
+2. print good mornint + the namespace
+*/
+
+string name = "satoshii";
+cout << "What is your name? "<< endl;
+cout << "Hello world " << name << endl;
     return 0;
 }
