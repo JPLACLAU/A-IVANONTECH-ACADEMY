262adc4f74df3564cf431e1db22babbeb3748a3b
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-07-23 06:16:57 -0300

    .

--- c++ hello world/Hello World/main.cpp
+++ c++ hello world/Hello World/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello everyone";
+    // Variable have types
+    //first type - (integer)
+    int a = 5;
+    int b = 3;
+    int c = a+b;
+    cout<<"Hello everyone"<< c;
     return 0;
 }
