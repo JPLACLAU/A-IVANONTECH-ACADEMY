41f93d7984cc8cb6b44920cfc3d58418cfe852c9
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-08-06 14:55:55 -0300

    .

--- c++ hello world/Hello World/main.cpp
+++ c++ hello world/Hello World/main.cpp
@@ -1,39 +1,32 @@
 // All writing here was done for excercise and practicing.
 // The excercises belong to Ivan on Tech Academy and other sources.
+// www.learncpp.com and CodeBeauty are some of the other sources I used to learncpp
 
+#include <array>
 #include <iostream>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
 
-using namespace std;
+namespace a::b::c
+{
+    inline constexpr std::string_view str{ "hello" };
+}
+
+template <class... T>
+std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args)
+{
+    return { sizeof...(T), (args + ...) };
+}
 
 int main()
 {
-  int numberOfAttempts = 0;
-  int theNumberToGuess = 0 ;
-  int currentGuess = 0;
+    auto [iNumbers, iSum]{ sum(1, 2, 3) };
+    std::cout << a::b::c::str << ' ' << iNumbers << ' ' << iSum << '\n';
 
-  cout << "Welcome to the game!" <<endl <<endl;
-  cout << "Player One: Please select a number:"<<endl;
-  cin >> theNumberToGuess;
+    std::array arr{ 1, 2, 3 };
 
-  while (true) {
-    cout << "Player 2, please guess the number:"<<endl;
-    cin >> currentGuess;
+    std::cout << std::size(arr) << '\n';
 
-    if(currentGuess>theNumberToGuess) {
-      cout<<"TOO HIGH! Guess lower!"<<endl;
-    }
-    else if(currentGuess<theNumberToGuess) {
-      cout<<"TOO LOW! Guess higher!"<<endl;
-    }
-    else {
-      cout<<"You guessed correctly! Congratulations!"<<endl;
-      break;
-    }
-
-  }
-
-
-
-
-  return 0;
+    return 0;
 }
