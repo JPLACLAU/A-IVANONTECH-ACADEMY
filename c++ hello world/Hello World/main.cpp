23e7a2cc5fe8ec6852f384400ec2a45d33081ade
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-08-10 17:58:36 -0300

    with bug

--- c++ hello world/Hello World/main.cpp
+++ c++ hello world/Hello World/main.cpp
@@ -4,10 +4,33 @@
 
 #include <iostream>
 
-int add(int x, int y); // needed so main.cpp knows that add() is a function declared elsewhere
+int add(int x, int y)
+{
+	return x + y;
+}
+
+void printResult(int z)
+{
+	std::cout << "The answer is: " << z << '\n';
+}
+
+int getUserInput()
+{
+	std::cout << "Enter a number: ";
+	int x{};
+	std::cin >> x;
+	return x;
+}
 
 int main()
 {
-    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
-    return 0;
+	int x{ getUserInput() };
+	int y{ getUserInput() };
+
+	std::cout << x << " + " << y << '\n';
+
+	int z{ add(x, 5) };
+	printResult(z);
+
+	return 0;
 }
