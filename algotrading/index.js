cc322615d2c51d54b782667d89bdfd4cd6ccbb2d
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-09-08 15:17:15 -0300

    .

--- algotrading/index.js
+++ algotrading/index.js
@@ -1,14 +1,14 @@
-console.log("welcome to index.js file baaaaaam");
+
 
 //Conection to the TESTING gemini platform
 const GeminiAPI = require("gemini-api").default;
 
 //These are just TESTING NOT REAL MONEY NOR WALLET KEYS
-var secret = "master-y9MQA0ZRcG1pSR9YflmS";
-var key ="gr5HdL21okk2LP2foSQF6ADzrFP";
+var secret = "21XxTWfsbd2ufXCZAZC1rVT2X3rq";
+var key ="account-iVHSkreYXowSK9kFCKts";
 
 
 const restClient = new GeminiAPI({key, secret, sandbox:true});
 
-restClient.newOrder({amount:10,price:100,side:"buy",symbols:"btcusd"});
+restClient.newOrder({amount:10, price:100, side:"buy", symbol:"btcusd"})
 .then(response => console.log(response));
