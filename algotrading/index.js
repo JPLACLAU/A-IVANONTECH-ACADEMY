22f19c016aa85fa0a3cb83c904fe5751df856b9e
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-09-08 20:10:10 -0300

    .

--- algotrading/index.js
+++ algotrading/index.js
@@ -5,7 +5,10 @@ const GeminiAPI = require("gemini-api").default;
 
 //These are just TESTING NOT REAL MONEY NOR WALLET KEYS
 var secret = "21XxTWfsbd2ufXCZAZC1rVT2X3rq";
-var key ="account-iVHSkreYXowSK9kFCKts";
+var key = "account-iVHSkreYXowSK9kFCKts";
+
+//This is a free api from crypto compare
+const CCAPIKey = "7036d42f4098c4553208f2b9bbdefd6880a6e3f0cf88f694ad2287d3dd649d81";
 
 
 const restClient = new GeminiAPI({key, secret, sandbox:true});
