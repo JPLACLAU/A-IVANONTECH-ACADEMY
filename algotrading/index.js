fb01950950fd35283d349fdbe020e8515c82caea
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-09-09 12:43:52 -0300

    .

--- algotrading/index.js
+++ algotrading/index.js
@@ -6,13 +6,12 @@ const GeminiAPI = require("gemini-api").default;
 const secret = "21XxTWfsbd2ufXCZAZC1rVT2X3rq";
 const key = "account-iVHSkreYXowSK9kFCKts";
 //This is a free api from crypto compare
-const CCAPIKey = "7036d42f4098c4553208f2b9bbdefd6880a6e3f0cf88f694ad2287d3dd649d81";
+const CrypAPIKey = "7036d42f4098c4553208f2b9bbdefd6880a6e3f0cf88f694ad2287d3dd649d81";
 const restClient = new GeminiAPI({key, secret, sandbox:true});
 
-const CryptoCompareAPI = require("cryptocompare");
-CryptoCompareAPI.setApikey(CCAPIKey)
-.catch(error => console.log(error));
 
+const CryptoCompareAPI = require("cryptocompare");
+CryptoCompareAPI.setApiKey(CrypAPIKey);
 
 
 restClient.newOrder({amount:10, price:100, side:"buy", symbol:"btcusd"})
