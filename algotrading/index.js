e37217b932d9d63d612e99eefe540614e91c3a7e
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-09-09 17:38:56 -0300

    .

--- algotrading/index.js
+++ algotrading/index.js
@@ -4,19 +4,17 @@ global.fetch = require("node-fetch");
 //Conection to the TESTING gemini platform
 const GeminiAPI = require("gemini-api").default;
 //These are just TESTING NOT REAL MONEY NOR WALLET KEYS
-const secret = "21XxTWfsbd2ufXCZAZC1rVT2X3rq";
-const key = "account-iVHSkreYXowSK9kFCKts";
+const secret = "account-iVHSkreYXowSK9kFCKts";
+const key = "21XxTWfsbd2ufXCZAZC1rVT2X3rq";
 //This is a free api from crypto compare
 const CrypAPIKey = "7036d42f4098c4553208f2b9bbdefd6880a6e3f0cf88f694ad2287d3dd649d81";
 const restClient = new GeminiAPI({key, secret, sandbox:true});
-
 
 const CryptoCompareAPI = require("cryptocompare");
 CryptoCompareAPI.setApiKey(CrypAPIKey);
 
 
-restClient.newOrder({amount:10, price:100, side:"buy", symbol:"btcusd"})
-
+restClient.newOrder({amount:10, price:100, side:"buy", symbol:"BTCUSDT"})
 .then(response => console.log(response))
 .catch(error => console.log(error));
 
