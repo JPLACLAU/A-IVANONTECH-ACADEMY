13034569a10615a167788d1cf4491c8f86cdd5de
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2021-09-09 08:23:33 -0300

    .

--- algotrading/index.js
+++ algotrading/index.js
@@ -10,8 +10,12 @@ var key = "account-iVHSkreYXowSK9kFCKts";
 //This is a free api from crypto compare
 const CCAPIKey = "7036d42f4098c4553208f2b9bbdefd6880a6e3f0cf88f694ad2287d3dd649d81";
 
-
 const restClient = new GeminiAPI({key, secret, sandbox:true});
+
+const CryptoCompareAPI = require("cryptocompare");
+CryptoCompareAPI.setApikey(CCAPIKey);
+
+
 
 restClient.newOrder({amount:10, price:100, side:"buy", symbol:"btcusd"})
 .then(response => console.log(response))
