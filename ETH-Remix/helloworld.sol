1b8247d8884691f3b4b75a026e5e400a1c5c819b
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2022-03-25 10:49:28 -0300

    .

--- ETH-Remix/helloworld.sol
+++ ETH-Remix/helloworld.sol
@@ -2,9 +2,15 @@ pragma solidity 0.7.5;
 //SPDX-License-Identifier: UNLICENSED
 
 contract HelloWorld {
+    //state variables
+    string message;
 
-    function hello() public pure returns(string memory){
-            return "Hello World";
+    constructor(string memory _message){
+        message = _message;
+    }
+
+    function hello() public returns(string memory){
+            return message;
     }
     
 }
