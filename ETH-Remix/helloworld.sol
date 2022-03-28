794b55846e4c27782816607ccb903d1f6ae0f072
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2022-03-28 13:50:54 -0300

    .

--- ETH-Remix/helloworld.sol
+++ ETH-Remix/helloworld.sol
@@ -3,6 +3,25 @@ pragma solidity 0.7.5;
 
 contract HelloWorld {
     //state variables
+    
+    struct Person{
+        uint age;
+        string name;
+    }
+
+    Person[] people;
+
+    function addPerson(uint _age, string memory _name) public {
+        Person memory newPerson = Person(_age, _name);
+        people.push(newPerson);
+    }
+
+    function getPerson(uint _index) public view returns (uint, string memory) {
+        Person memory personToReturn = people[_index];
+        return (personToReturn.age, personToReturn.name);
+    }
+
+
     string message;
 
     constructor(string memory _message){
