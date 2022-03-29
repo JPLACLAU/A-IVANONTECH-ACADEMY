ac464b09e935033a6dd4484b30a9bf52996bff9d
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2022-03-29 14:54:54 -0300

    .

--- ETH-Remix/HungerGames/HungerGames.sol
+++ ETH-Remix/HungerGames/HungerGames.sol
@@ -3,30 +3,31 @@ pragma solidity 0.8.13;
 
 contract HungerGames {
     //state variables
+    uint256 public peopleCount = 0;
+    mapping(uint => Person) public people;
+
     
     struct Person{
-        uint age;
-        string name;
-        uint weight; //this value is in integer kilograms. 
+        uint256 _id;
+        uint _age;
+        string _name;
+        uint _weight; //this value is in integer kilograms. 
         //IF you weith 105,5 kilogramos, you round down to 105 kg. If you weight 105,6kg, round up to 106kg.
-        uint month;
-        uint investedmoney;
+        uint _month;
+        uint _investedmoney;
     }
 
-    Person[] people;
 
-    uint256 public peopleCount;
-
-    function addPerson(uint _age, string memory _name, uint _weight, uint _month, uint _investedmoney) public {
-        Person memory newPerson = Person(_age, _name, _weight, _month, _investedmoney);
-        people.push(newPerson);
+    function addPerson(uint256 _id, uint _age, string memory _name, uint _weight, uint _month, uint _investedmoney)
+     public {
         peopleCount += 1;
+        people[peopleCount] = Person(_id , _age, _name, _weight, _month, _investedmoney);
     }
 
-    function getPerson(uint _index) public view returns (uint, string memory, uint, uint, uint) {
+    function getPerson(uint _index) public view returns (uint256, uint, string memory, uint, uint, uint) {
         Person memory personToReturn = people[_index];
-        return (personToReturn.age, personToReturn.name, personToReturn.weight, personToReturn.month,
-         personToReturn.investedmoney);
+        return (personToReturn._id, personToReturn._age, personToReturn._name, personToReturn._weight, 
+        personToReturn._month, personToReturn._investedmoney);
         
     }
   
