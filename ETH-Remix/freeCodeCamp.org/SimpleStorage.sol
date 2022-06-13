5d7767419a4ca933b6ed369f6f0ad838eb322d1b
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2022-06-13 11:31:58 -0300

    .

--- ETH-Remix/freeCodeCamp.org/SimpleStorage.sol
+++ ETH-Remix/freeCodeCamp.org/SimpleStorage.sol
@@ -7,6 +7,10 @@ contract SimpleStorage {
     function store (uint256 _favoriteNumber) public{ 
         favoriteNumber = _favoriteNumber;
     }
+// view, pure
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
   
 
 }
