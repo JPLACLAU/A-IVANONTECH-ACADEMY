f948b6e13a0d8e548f0296f12b2b4be8b6f47a94
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2022-06-13 11:06:07 -0300

    a

---
+++ ETH-Remix/freeCodeCamp.org/SimpleStorage.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.13;
+
+contract SimpleStorage {
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = false;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favoriteAddress = xxx;
+    bytes32 favoriteBytes = "cat";
+
+}
