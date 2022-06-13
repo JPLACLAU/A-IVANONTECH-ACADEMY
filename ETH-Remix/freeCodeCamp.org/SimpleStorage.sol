0b7d5473174025dae6d3305116ad07b5f826ae44
Jean Paul Laclau <laclaujeanpaul@gmail.com>
2022-06-13 11:15:05 -0300

    .

--- ETH-Remix/freeCodeCamp.org/SimpleStorage.sol
+++ ETH-Remix/freeCodeCamp.org/SimpleStorage.sol
@@ -2,7 +2,7 @@ pragma solidity 0.8.13;
 //SPDX-License-Identifier: UNLICENSED
 
 contract SimpleStorage {
-    uint256 favoriteNumber; // this wil get initialized to 0
+    uint256 public favoriteNumber; // this wil get initialized to 0
 
     function store (uint256 _favoriteNumber) public{ 
         favoriteNumber = _favoriteNumber;
