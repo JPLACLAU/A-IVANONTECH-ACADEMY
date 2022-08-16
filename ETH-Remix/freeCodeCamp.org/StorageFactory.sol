e01b55e4cfebef5d855609303292febd65aae3f7
JPLACLAU <laclaujeanpaul@gmail.com>
2022-08-16 14:07:16 -0300

    .

--- ETH-Remix/freeCodeCamp.org/StorageFactory.sol
+++ ETH-Remix/freeCodeCamp.org/StorageFactory.sol
@@ -12,4 +12,16 @@ contract StorageFactory {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address
+        // ABI aplication binary interface
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
 }
