fa5d07296664524d1a12e234bf03194edbfcd186
JPLACLAU <laclaujeanpaul@gmail.com>
2022-08-18 03:56:02 -0300

    .

--- ETH-Remix/freeCodeCamp.org/Lesson 4/FundMe.sol
+++ ETH-Remix/freeCodeCamp.org/Lesson 4/FundMe.sol
@@ -2,12 +2,25 @@ pragma solidity 0.8.8;
 //SPDX-License-Identifier: UNLICENSED
 
 contract FundMe {
+
+    uint256 public minimumUsd = 50;
+
     function fund() public payable{
         // Want to be able to set a minimum fund amount ins usd
         //1. how to we sent eth to this contract?
-        require(msg.value >1e18, "Didn't send enought!");
+        require(msg.value >= minimumUsd, "Didn't send enought!");
         // 1e18 = 1*10*18 == 10000000000000000000
         // 1ETH = 1e18 wei money has to be written in wei or poligon or wharever"
+
+    }
+
+    function getPrice() public {
+        // ABI
+        // Address goerli  0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e ethusd
+
+    }
+
+    function getConversionRate() public {
 
     }
 
