c8de1f7ee0e8551279f3b8f8668ce1a5f4247a09
JPLACLAU <laclaujeanpaul@gmail.com>
2022-08-19 16:37:43 -0300

    z

--- ETH-Remix/freeCodeCamp.org/Lesson 4/FundMe.sol
+++ ETH-Remix/freeCodeCamp.org/Lesson 4/FundMe.sol
@@ -12,18 +12,26 @@ contract FundMe {
         // Want to be able to set a minimum fund amount ins usd
         //1. how to we sent eth to this contract?
         require(msg.value >= minimumUsd, "Didn't send enought!");
-        // 1e18 = 1*10*18 == 10000000000000000000
+        // 1e18 = 1*10*18 == 1000000000000000000
         // 1ETH = 1e18 wei money has to be written in wei or poligon or wharever"
 
     }
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256) {
         // ABI
         // Address goerli  0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e ethusd
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 1800.00000000
+        return uint256 (price * 1e10); // 1**10 == 10000000000
 
     }
 
-    function getConversionRate() public {
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) /1e18;
+        return ethAmountInUsd;
 
     }
 
