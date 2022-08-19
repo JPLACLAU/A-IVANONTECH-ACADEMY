ff64a4b1fca85e306e9d3c78a454a6b7251c99c9
JPLACLAU <laclaujeanpaul@gmail.com>
2022-08-19 17:45:37 -0300

    z

--- ETH-Remix/freeCodeCamp.org/Lesson 4/FundMe.sol
+++ ETH-Remix/freeCodeCamp.org/Lesson 4/FundMe.sol
@@ -1,12 +1,15 @@
 pragma solidity 0.8.8;
 //SPDX-License-Identifier: UNLICENSED
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 50;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable{
         // Want to be able to set a minimum fund amount ins usd
@@ -14,25 +17,11 @@ contract FundMe {
         require(msg.value >= minimumUsd, "Didn't send enought!");
         // 1e18 = 1*10*18 == 1000000000000000000
         // 1ETH = 1e18 wei money has to be written in wei or poligon or wharever"
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
 
     }
 
-    function getPrice() public view returns(uint256) {
-        // ABI
-        // Address goerli  0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e ethusd
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // ETH in terms of USD
-        // 1800.00000000
-        return uint256 (price * 1e10); // 1**10 == 10000000000
 
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) /1e18;
-        return ethAmountInUsd;
-
-    }
 
 }
---
+++ ETH-Remix/freeCodeCamp.org/Lesson 4/PriceConverter.sol
@@ -0,0 +1,34 @@
+pragma solidity 0.8.8;
+//SPDX-License-Identifier: UNLICENSED
+
+// library
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter {
+
+        function getPrice() public view returns(uint256) {
+        // ABI
+        // Address goerli  0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e ethusd
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 1800.00000000
+        return uint256 (price * 1e10); // 1**10 == 10000000000
+
+    }
+
+        function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) /1e18;
+        return ethAmountInUsd;
+
+    }
+
+}
