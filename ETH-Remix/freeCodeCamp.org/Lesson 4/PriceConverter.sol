71f0d03123e170eb627c3c52b1b2000b13c2208e
JPLACLAU <laclaujeanpaul@gmail.com>
2022-08-20 03:05:55 -0300

    z

--- ETH-Remix/freeCodeCamp.org/Lesson 4/PriceConverter.sol
+++ ETH-Remix/freeCodeCamp.org/Lesson 4/PriceConverter.sol
@@ -8,7 +8,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
 
-        function getPrice() public view returns(uint256) {
+        function getPrice() internal view returns(uint256) {
         // ABI
         // Address goerli  0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e ethusd
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
@@ -19,12 +19,12 @@ library PriceConverter {
 
     }
 
-        function getVersion() public view returns (uint256){
+        function getVersion() internal view returns (uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) /1e18;
         return ethAmountInUsd;
---
+++ ETH-Remix/freeCodeCamp.org/Lesson 4/SafeMathTester.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.0;
+
+
+contract SafeMathTester{
+
+    uint8 public bigNumber = 255;
+
+    
+
+}
