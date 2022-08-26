b057871ff562a3bd40a1b1d2510760897711b58d
JPLACLAU <laclaujeanpaul@gmail.com>
2022-08-26 16:53:43 -0300

    z

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
-require("dotenv").config;
+require("dotenv");
 
 async function main() {
   // compile them in our code
@@ -9,7 +9,6 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
     "HTTP://127.0.0.1:7545"
   );
-
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   // pasting private key here its a huge nono, but this is ganache
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
@@ -20,7 +19,7 @@ async function main() {
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log("Deploying, please wait...");
   const contract = await contractFactory.deploy(); //stop here, wait contract to deploy
-  console.log(contract); // 7:10
+  console.log(contract);
 }
 
 main()
