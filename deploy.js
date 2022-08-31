90a5a99315e1c04e2c2e27f3a386481302a4a502
JPLACLAU <laclaujeanpaul@gmail.com>
2022-08-31 15:24:49 -0300

    z

--- deploy.js
+++ deploy.js
@@ -7,13 +7,14 @@ async function main() {
   // compile them separately
   // http://127.0.0.1:7545
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-    encryptedJson,
-    process.env.PRIVATE_KEY_PASSWORD
-  );
-  wallet = await wallet.connect(provider);
+  //const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  //let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //  encryptedJson,
+  //  process.env.PRIVATE_KEY_PASSWORD
+  //);
+  //wallet = await wallet.connect(provider);
 
   // pasting private key here its a huge nono, but this is ganache
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
@@ -25,6 +26,8 @@ async function main() {
   console.log("Deploying, please wait...");
   const contract = await contractFactory.deploy(); //stop here, wait contract to deploy
   console.log(contract);
+  await contract.deployTransaction.wait(1);
+  console.log(`Contract Address: ${contract.address}`);
 }
 
 main()
