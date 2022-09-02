55d834af16514d25dc5b663b012cb057b2798f5a
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-02 15:30:04 -0300

    z

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+//const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+//const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const { GOERLI_RPC_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.7",
+    // defaultNetwork: "hardhat", // this fake hardhat network with rpcurl and pkey
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+
+    solidity: "0.8.7",
 };
