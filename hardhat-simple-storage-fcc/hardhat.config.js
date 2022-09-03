97abdcae3345366ac5b3ae467cf66ee8b5bd8601
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-03 16:37:13 -0300

    z

--- hardhat-simple-storage-fcc/hardhat.config.js
+++ hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 //const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 //const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const { GOERLI_RPC_URL, PRIVATE_KEY } = process.env;
+const { GOERLI_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
     // defaultNetwork: "hardhat", // this fake hardhat network with rpcurl and pkey
@@ -19,4 +20,7 @@ module.exports = {
     },
 
     solidity: "0.8.7",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
--- hardhat-simple-storage-fcc/scripts/deploy.js
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,5 +1,5 @@
 //imports
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 //async main
 async function main() {
@@ -10,6 +10,38 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy();
     await simpleStorage.deployed();
     console.log(`Deployed contract to: ${simpleStorage.address}`);
+    console.log(network.config);
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block txes...");
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
+    } // == es igual a, ===
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log(`Current Value is: ${currentValue}`);
+
+    // update current value
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log(`Updated Value is: ${updatedValue}`);
+}
+async function verify(contractAddress, args) {
+    //it works on etherscan, noto in all scanners
+    //we use hardhat plugins instead ot etherscan tutorial: hardhat-etherscan
+    console.log("Verifying contract...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowercase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(e);
+        }
+    }
 }
 
 // main
