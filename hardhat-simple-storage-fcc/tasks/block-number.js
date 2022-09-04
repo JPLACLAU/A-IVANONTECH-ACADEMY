c0733629bfcf5525206eb7d3a9a9fe8d091d4ea2
JPLACLAU <laclaujeanpaul@gmail.com>
2022-09-04 17:43:41 -0300

    z

--- hardhat-simple-storage-fcc/tasks/block-number.js
+++ hardhat-simple-storage-fcc/tasks/block-number.js
@@ -1,8 +1,12 @@
-const { task } = require("hardhag/config");
+const { task } = require("hardhat/config");
 
-task("block.number", "Prints the current block number").setAction(
+task("block-number", "Prints the current block number").setAction(
+    // const blockTask = async function() => {}
+    // async function blockTask() {}
     async (taskArgs, hre) => {
         const blockNumber = await hre.ethers.provider.getBlockNumber();
         console.log(`Current block number: ${blockNumber}`);
     }
 );
+
+module.exports = {};
